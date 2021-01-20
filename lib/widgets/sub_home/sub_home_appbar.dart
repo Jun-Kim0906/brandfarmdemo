48fab7a5876fe5ca7b59357d9fe3ee9cec28d1d4
김준우 <nmnmko04@gmail.com>
2021-01-20 15:05:04 +0900

    [UPDATE] 홈화면 재구성

---
+++ lib/widgets/sub_home/sub_home_appbar.dart
@@ -0,0 +1,59 @@
+import 'package:BrandFarm/widgets/brandfarm_icons.dart';
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+
+class SubHomeAppBar extends StatelessWidget implements PreferredSizeWidget{
+  const SubHomeAppBar({
+    Key key, this.iconPressed, this.notificationPressed, this.settingPressed,
+  }) : super(key: key);
+  
+  final Function iconPressed;
+  final Function notificationPressed;
+  final Function settingPressed;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: [
+              IconButton(
+                iconSize: 60.0,
+                icon: Image.asset('assets/brandfarm.png'),
+                onPressed: iconPressed,
+              ),
+              Spacer(),
+              Badge(
+                position: BadgePosition.topEnd(top: 2, end: 8),
+                badgeContent: Text(
+                  '2',
+                  style: TextStyle(color: Colors.white, fontSize: 14.0),
+                ),
+                child: IconButton(
+                  iconSize: 40.0,
+                  icon: Icon(
+                    Icons.notifications_none_sharp,
+                  ),
+                  onPressed: notificationPressed,
+                ),
+                padding: EdgeInsets.all(4.5),
+              ),
+              IconButton(
+                  iconSize: 35.0,
+                  icon: Icon(
+                    BrandFarmIcons.settings,
+                  ),
+                  onPressed: settingPressed)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
